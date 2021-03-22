ea32aae4aeb975b0aa01cf1c62dc798bd9114ab3
natalieberko <70112896+natalieberko@users.noreply.github.com>
2021-03-22 13:27:32 -0600

    adding button animations with jquery

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ function fade() {
     $('.preloader').fadeOut("slow");
 }
 setTimeout(fade, 1000);
+
+$(document).ready(function(){
+$(".viewProject").hover(function(){
+    $(this).animate({ width: "50px" , height: "50px"});
+}, function() {
+    $(this).animate({ width: "100px", height: "100px" });
+});
+});
