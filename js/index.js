8000d2b37aa9a050799c2eb78726de5c604beeee
natalieberko <70112896+natalieberko@users.noreply.github.com>
2021-03-22 16:31:23 -0600

    css styling for pillboxes/ sticky header
    added pill boxes to html and styled in css
    tried to resolve sticky header situation as well.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,73 @@
+$(document).ready(function(){
 console.log("Your index.js file is loaded correctly!");
 function fade() {
     $('.preloader').fadeOut("slow");
 }
 setTimeout(fade, 1000);
 
-$(document).ready(function(){
+
 $(".viewProject").hover(function(){
-    $(this).animate({ width: "50px" , height: "50px"});
-}, function() {
-    $(this).animate({ width: "100px", height: "100px" });
+    $(this).animate({
+        width: "229px"
+    });
+}, function(){
+    $(this).animate({
+        width: "152px"
+    });
 });
+
+var stickyNav = $("header").offset().top; 
+var sticky = function(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop>stickyNav){
+        $("header").addClass("sticky");
+    }else {
+        $("header").removeClass("sticky");
+    }
+}
+sticky();
+$(window).scroll(function(){
+    sticky();
+});
+});
+//this function works, but its alignment changes on scroll//
+//hero image alignment also changes on scroll//
+$("a").hover(function(){
+    $(this).css({
+        color: "#6973ED"
+    });
+}, function(){
+    $(this).css({
+        color: "black"
+    });
+});
+
+$("li").hover(function(){
+    $(this).css({
+        color: "#6973ED"
+    });
+}, function(){
+    $(this).css({
+        color: "#222222"
+    });
+});
+
+$("#seeWork").hover(function(){
+    $(this).animate({
+        width: "229px"
+    });
+}, function(){
+    $(this).animate({
+        width: "152px"
+    });
+});
+
+$("#email").hover(function(){
+    $(this).css({
+        color: "#000000"
+    });
+}, function(){
+    $(this).css({
+        color: "#333333"
+    });
 });
