2881f337c54cd3973b8dd7009e2c721b419bff92
natalieberko <70112896+natalieberko@users.noreply.github.com>
2021-03-22 18:03:26 -0600

    more styling

--- js/aboutme.js
+++ js/aboutme.js
@@ -4,3 +4,38 @@ function fade() {
 }
 setTimeout(fade, 1000);
 
+var stickyNav = $("header").offset().top; 
+var sticky = function(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop>stickyNav){
+        $("header").addClass("sticky");
+    }else {
+        $("header").removeClass("sticky");
+    }
+}
+sticky();
+$(window).scroll(function(){
+    sticky();
+});
+//this function works, but its alignment changes on scroll//
+//hero image alignment also changes on scroll//
+
+$("a").hover(function(){
+    $(this).css({
+        color: "#6973ED"
+    });
+}, function(){
+    $(this).css({
+        color: "black"
+    });
+});
+
+$("#email").hover(function(){
+    $(this).css({
+        color: "#000000"
+    });
+}, function(){
+    $(this).css({
+        color: "#333333"
+    });
+});
